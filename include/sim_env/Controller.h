e28f7581a12fdf5e37728327ecb26bc7b9bb0b53
Haoran Song <hsongad@connect.ust.hk>
2018-09-21 15:42:37 +0200

    Correct Controller Clone, used for parallel running world

--- include/sim_env/Controller.h
+++ include/sim_env/Controller.h
@@ -140,7 +140,7 @@ namespace sim_env {
     class PIDController : public Controller {
     public:
         PIDController(float kp=1.0, float ki=0.1, float kd=0.0);
-        ~PIDController();
+        virtual ~PIDController();
         void setKp(float kp);
         void setKi(float ki);
         void setKd(float kd);
@@ -199,6 +199,15 @@ namespace sim_env {
         virtual ~RobotVelocityController() = 0;
         virtual unsigned int getTargetDimension() = 0;
         virtual void setTargetVelocity(const Eigen::VectorXf& velocity) = 0;
+        virtual RobotPtr getRobot() const = 0;
+        /**
+         * Clone this robot controller given that the controller will operate on the
+         * robot with the same name in the given world. This allows you to clone this controller
+         * and run it on a cloned world.
+         * @param world
+         * @return
+         */
+        virtual RobotVelocityControllerPtr clone(WorldPtr world) = 0;
         virtual bool control(const Eigen::VectorXf& positions,
                              const Eigen::VectorXf& velocities,
                              float timestep,
@@ -214,6 +223,7 @@ namespace sim_env {
         RobotPositionController(RobotPtr robot, RobotVelocityControllerPtr velocity_controller);
         ~RobotPositionController();
         void setTargetPosition(const Eigen::VectorXf& position);
+        virtual RobotPtr getRobot() const;
         bool control(const Eigen::VectorXf& positions,
                      const Eigen::VectorXf& velocities,
                      float timestep,
